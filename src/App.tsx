9c8b8c9d8f117dfa8d46a197e088f20ee9fc2236
Kamesas <semakaleksandr2014@gmail.com>
2019-10-28 19:43:20 +0200

    simple example is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { observer } from "mobx-react";
+import TodoStore from "./store/TodoStore";
 
-const App: React.FC = () => {
+const App: React.FC = observer(() => {
+  const { todos, remainingTodos, toggleTodo } = useContext(TodoStore);
+
   return (
     <div className="App">
       <h3>MobX with hooks starter</h3>
+      <div>Is done => {remainingTodos}</div>
+      {todos.map((item, i) => (
+        <div key={item.id} onClick={() => toggleTodo(i)}>
+          {item.text}
+        </div>
+      ))}
     </div>
   );
-};
+});
 
 export default App;
---
+++ src/store/TodoStore.ts
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+import { decorate, observable, computed } from "mobx";
+
+export class Todos {
+  todos = [{ id: 1, text: "Buy eggs", completed: true }, { id: 2, text: "Write a post", completed: false }];
+
+  get remainingTodos() {
+    return this.todos.filter(t => !t.completed).length;
+  }
+
+  toggleTodo = (index: number) => {
+    this.todos[index].completed = !this.todos[index].completed;
+  };
+}
+
+decorate(Todos, {
+  todos: observable,
+  remainingTodos: computed
+});
+
+export default createContext(new Todos());
