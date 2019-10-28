fc7d54b11af1a5707ecc42cb7368045ca2b5b8fb
Kamesas <semakaleksandr2014@gmail.com>
2019-10-28 12:16:07 +0200

    some update npm packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {Provider} from "mobx-react";
-import {autorun, configure} from "mobx";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "mobx-react";
+import { autorun, configure } from "mobx";
 import { store } from "./store";
 
-configure({enforceActions: "always"});
+configure({ enforceActions: "always" });
 
 autorun(() => {
-  ReactDOM.render(<Provider {...store}>
-    <App/>
-  </Provider>, document.getElementById("root"));
+  ReactDOM.render(
+    <Provider {...store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
 });
-
 
 //ReactDOM.render(<App/>, document.getElementById("root"));
