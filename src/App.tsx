285b8a02950004b4ae3bde4d87f688a9c3115b45
Kamesas <semakaleksandr2014@gmail.com>
2019-11-19 22:09:05 +0200

    simple datePicker is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import dayJS from "dayjs";
 
 const App: React.FC = () => {
+  const [day, setDay] = useState<any>(dayJS());
+  const [visibilityDatePicker, setVisibilityDatePicker] = useState(false);
+
+  const renderMonth = (m: any) => {
+    const mondayOfNowWeek = m
+      .clone()
+      .startOf("month")
+      .day(1);
+
+    const lastDayOfWeekInNowMonth = m
+      .clone()
+      .endOf("month")
+      .day(7)
+      .format("DD MM YYYY");
+
+    let week = [];
+
+    let i = 0;
+    do {
+      week.push(mondayOfNowWeek.clone().add(i, "day"));
+      i++;
+    } while (
+      week[week.length - 1].format("DD MM YYYY") !== lastDayOfWeekInNowMonth
+    );
+
+    return week.map((day, i) => {
+      return (
+        <div
+          className="day"
+          key={day.format("DD MM YYYY")}
+          onClick={() => console.log(day.format("DD-MM-YYYY"))}
+        >
+          {day.date()}
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <input type="text" onFocus={() => setVisibilityDatePicker(true)} />
+
+      {visibilityDatePicker && (
+        <div
+          className="datePicker"
+          tabIndex={0}
+          onBlur={() => setVisibilityDatePicker(false)}
         >
-          Learn React
-        </a>
-      </header>
+          <button onClick={() => setDay(day.subtract(1, "month"))}>
+            minus
+          </button>
+          <div>{day.format("MMMM")}</div>
+          <button onClick={() => setDay(day.add(1, "month"))}>add</button>
+          <div className="month">{renderMonth(day)}</div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
