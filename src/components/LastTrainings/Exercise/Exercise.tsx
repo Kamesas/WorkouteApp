39853a07e35b2807f7e2d4244220fd16037b5d73
Kamesas <semakaleksandr2014@gmail.com>
2019-12-31 15:50:51 +0200

    a bit refactored something

--- src/components/LastTrainings/Exercise/Exercise.tsx
+++ src/components/LastTrainings/Exercise/Exercise.tsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
-import {
-  FaChevronCircleDown,
-  FaChevronCircleUp,
-  FaDivide
-} from "react-icons/fa";
+import { FaChevronCircleDown, FaChevronCircleUp } from "react-icons/fa";
 import "./Exercise.scss";
 
 interface IProps {
