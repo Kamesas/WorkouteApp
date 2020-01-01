4b953cd70b839e9c1d628547df7a58eb04492285
Kamesas <semakaleksandr2014@gmail.com>
2020-01-01 14:41:36 +0200

    added .enb file for Firebase's API key

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -5,9 +5,59 @@ interface IProps {
 }
 
 const Auth: React.FC<IProps> = () => {
+  const loginHandler = () => {
+    const registerBody = {
+      login: "test",
+      email: "testing4@gmal.com",
+      password: "12345678",
+      returnSecureToken: true
+    };
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify(registerBody)
+      }
+    )
+      .then(res => console.log("res", res.json()))
+      .catch(err => console.log("err", err));
+  };
+
+  const registerHandler = () => {
+    const registerBody = {
+      login: "test",
+      email: "testing4@gmal.com",
+      password: "12345678",
+      returnSecureToken: true
+    };
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify(registerBody)
+      }
+    )
+      .then(res => console.log("res", res))
+      .catch(err => console.log("err", err));
+  };
+
   return (
     <div className="Auth">
       <div>Auth</div>
+
+      <button onClick={registerHandler}>register</button>
+      <button onClick={loginHandler}>login</button>
+
+      <form>
+        <input type="text" placeholder="login" />
+        <input type="text" placeholder="password" />
+        <input type="text" placeholder="email" />
+      </form>
     </div>
   );
 };
