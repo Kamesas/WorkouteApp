5c77e3c572203269c5d5959a0f1468cfd8837b4e
Kamesas <semakaleksandr2014@gmail.com>
2020-02-23 18:01:33 +0200

    added some styles for CV page

--- src/pages/CV/CV.tsx
+++ src/pages/CV/CV.tsx
@@ -2,32 +2,75 @@ import React from "react";
 import "./CV.scss";
 
 const CV = () => {
+  const birthday = "1988/06/10";
+  function getAge(dateString: string) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+
   return (
     <div className="CV">
-      <div>ФИО: Семак Александр Сергеевич.</div>
-      <div> Рожден: 10.06.1988г (30 лет).</div>
-      <ul>
-        <li> Skype: kamesas1</li>
-        <li> Телефон: +38 (099) 770 81 64</li>
-        <li> Email: semakaleksandr2014@gmail.com</li>
-        <li> GitHub: https://github.com/Kamesas</li>
-      </ul>
-      <div>
-        Образование:
-        <ul>
-          <li>школа - ОСШ I-III ступеней;</li>
-          <li>колледж - Запорожский медицинский колледж (фельдшер);</li>
-          <li>
-            университет - Запорожский национальный университет (реабилитолог)
+      <div className="CV-title">Semak Alexandr</div>
+      <div className="CV-birthday">10.06.1988 ({getAge(birthday)} years)</div>
+      <div className="CV-expirience">
+        Work experience in web-dev more than a year
+      </div>
+
+      <hr />
+      <div className="CV-contacts">
+        <div className="CV-contacts-title">Contacts:</div>
+        <ul className="CV-contacts-list">
+          <li className="CV-contacts-item">
+            <span className="fieldType">Skype:</span> kamesas1
+          </li>
+          <li className="CV-contacts-item">
+            <span className="fieldType">Tel:</span> +38 (099) 770 81 64
+          </li>
+          <li className="CV-contacts-item">
+            <span className="fieldType">Email: </span>
+            semakaleksandr2014@gmail.com
+          </li>
+          <li className="CV-contacts-item">
+            <span className="fieldType">GitHub: </span>
+            <a
+              href=" https://github.com/Kamesas"
+              target="_blanck"
+              className="CV-contacts-link"
+            >
+              github.com/Kamesas
+            </a>
           </li>
         </ul>
       </div>
-      <div>
-        Знаю:
-        <ul>
-          <li>JavaScript, TypeScript</li>
-          <li>React, Redux, Mobx, Gatsby</li>
-          <li>HTML, CSS, SASS, Bootstrap</li>
+
+      <div className="CV-education">
+        <div className="CV-education-title"> Education:</div>
+        <ul className="CV-education-list">
+          <li className="CV-education-item">School for grades 1-11</li>
+          <li className="CV-education-item">
+            <a
+              href="https://www.znu.edu.ua/frontpage/3594.eng.html"
+              target="_blanck"
+              className="CV-education-link"
+            >
+              Zaporizhzhia National University (Faculty of Physical Education)
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="CV-skills">
+        <div className="CV-skills-title">Skills:</div>
+        <ul className="CV-skills-list">
+          <li className="CV-skills-item">JavaScript, TypeScript</li>
+          <li className="CV-skills-item">React, Redux, Mobx, Gatsby</li>
+          <li className="CV-skills-item">HTML, CSS, SASS, Bootstrap</li>
         </ul>
       </div>
     </div>
