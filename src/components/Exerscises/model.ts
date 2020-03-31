8c0e4fe74bd533ed2947dc4736461195dc51a759
Kamesas <semakaleksandr2014@gmail.com>
2020-03-31 10:02:01 +0300

    added couple exercises in list

--- src/components/Exerscises/model.ts
+++ src/components/Exerscises/model.ts
@@ -1,5 +1,8 @@
 export const exercises = [
   { title: "Push ups", alias: "pushUps" },
   { title: "Pull ups", alias: "pullUps" },
-  { title: "Squats", alias: "squats" }
+  { title: "Squats", alias: "squats" },
+  { title: "Crunches", alias: "crunches" },
+  { title: "Biceps", alias: "biceps" },
+  { title: "Hyperextension ", alias: "hyperextension" }
 ];
